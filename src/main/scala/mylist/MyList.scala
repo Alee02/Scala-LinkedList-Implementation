d7de42c4734cbcd8c661a57f21b96d71ca50707e
Alee02 <a-shah@hotmail.co.uk>
2019-05-06 22:30:11 +0100

    Added concatonate method

--- src/main/scala/mylist/MyList.scala
+++ src/main/scala/mylist/MyList.scala
@@ -11,8 +11,9 @@ abstract class MyList[+A] {
   }
 
   def map[B](transformer: Function1[A, B]): MyList[B]
-//  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
   def filter(predicate: MyPredicate[A]): MyList[A]
+  def ++[B >: A](list: MyList[B]): MyList[B]
 }
 
 class Empty[A]
@@ -31,9 +32,11 @@ object Empty extends MyList {
 
   override def map[B](transformer: Function1[Nothing, B]): MyList[B] = Empty
 
-//  override def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+  override def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
 
   override def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+
+  override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -51,12 +54,23 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     new Cons[B](transformer(h), t.map(transformer))
   }
 
-//  override def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] = ???
+  /*
+  [1, 2].flatMap(n => [n, n+1])
+  = [1, 2] ++ [2].flatMap(n => [n, n+1])
+  = [1, 2] ++ [2,3] ++ Empty.flatMap(n => [n, n+1])
+  = [1, 2] ++ [2, 3] ++ Empty
+  = [1, 2, 2, 3]
+   */
+  override def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] = {
+    transformer.transform(h) ++ t.flatMap(transformer)
+  }
 
   override def filter(predicate: MyPredicate[A]): MyList[A] = {
     if (predicate.test(h)) new Cons(h, t.filter(predicate))
     else t.filter(predicate)
   }
+
+  override def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
 }
 
 trait MyPredicate[-T] {
@@ -71,5 +85,9 @@ object ListTest extends App {
 
   val list = new Cons(true, new Cons(true , new Cons(true, Empty)))
   val listOfIntegers = new Cons(1, new Cons(2 , new Cons(3, Empty)))
-  println(listOfIntegers.map(a => a * 2))
+
+  println(listOfIntegers.filter(new MyPredicate[Int] {
+    override def test(element: Int): Boolean = element < 2
+  }))
+
 }
